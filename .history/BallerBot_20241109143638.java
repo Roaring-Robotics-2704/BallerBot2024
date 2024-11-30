1a889168ede73f6b57dbf5fb7663044aee14cce1
cordelia34 <corh2945@icloud.com>
2024-11-30 11:00:20 -0600

    Archetecture Added

--- .history/BallerBot_20241109143638.java
+++
@@ -1,9 +0,0 @@
-public class BallerBot{
-    public static void main(String[]args){
-        System.out.println("Hello World!");
-        System.out.println("GOAT MILK");
-=======
-        System.out.println("Hoh hoh hoh");
->>>>>>> 54178bce3d2940d9d37b532d050734165b4325fc
-    }
-}
--- .history/BallerBot_20241109143717.java
+++
@@ -1,9 +0,0 @@
-public class BallerBot{
-    public static void main(String[]args){
-        System.out.println("Hello World!");
-        System.out.println("Hoh hoh hoh");
-
-        System.out.println("GOAT MILK");
-
-    }
-}
