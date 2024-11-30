1321ca5aca56121baa27f765a911d5029132ef4b
LasserCodes <radrocket09@gmail.com>
2024-11-30 12:13:34 -0600

    Update BallerBot.java

--- BallerBot.java
+++ BallerBot.java
@@ -1,30 +1,10 @@
+import Subsystems.ConfettiLauncher;
+
+
 public class BallerBot{
+    private static ConfettiLauncher confettiLauncher=new ConfettiLauncher();
     public static void main(String[]args){
-        System.out.println("Hello World!");
-
-        System.out.println("Ok now, goodbye.");
-
-
-        System.out.println("SahaSwiftie");
-
-        System.out.println("Hoh hoh hoh");
-
-
-
-
-        System.out.println("roshan the seagulll");
-
-
-
-
-        System.out.println("tanmay is cringe");
-        System.out.println("From the scereen to the ring to the pen to the king got my crown thats my bling.");
-        System.out.println("I see you roshan");
-
-
-        
-        System.out.println("GOAT MILK");
-
+        confettiLauncher.ConfettiLaunched();
 
     }
 }
