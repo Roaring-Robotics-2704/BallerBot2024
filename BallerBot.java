11b26713d9ac0615765ec0ed5d7580b4e5fdd4fc
Michael <mmarchiniakjr@hotmail.com>
2024-11-30 12:36:06 -0600

    fix code so it actually runs

--- BallerBot.java
+++ BallerBot.java
@@ -1,32 +1,59 @@
-import Subsystems.Camera;
-
 public class BallerBot{
     public static void main(String[]args){
-        // System.out.println("Hello World!");
-
-        // System.out.println("Ok now, goodbye.");
-
-
-        // System.out.println("SahaSwiftie");
-
-        // System.out.println("Hoh hoh hoh");
-
-
-
-
-        // System.out.println("roshan the seagulll");
-
-
-
-
-        // System.out.println("tanmay is cringe");
-        // System.out.println("From the scereen to the ring to the pen to the king got my crown thats my bling.");
-        // System.out.println("I see you roshan");
-
-
-        
-        // System.out.println("GOAT MILK");
-
+      // We're no strangers to love
+      // You know the rules and so do I
+      // A full commitment's what I'm thinkin' of
+      // You wouldn't get this from any other guy
+      // I just wanna tell you how I'm feeling
+      // Gotta make you understand
+      // Never gonna give you up
+      // Never gonna let you down
+      // Never gonna run around and desert you
+      // Never gonna make you cry
+      // Never gonna say goodbye
+      // Never gonna tell a lie and hurt you
+      // We've known each other for so long
+      // Your heart's been aching, but you're too shy to say it
+      // Inside, we both know what's been going on
+      // We know the game and we're gonna play it
+      // And if you ask me how I'm feeling
+      // Don't tell me you're too blind to see
+      // Never gonna give you up
+      // Never gonna let you down
+      // Never gonna run around and desert you
+      // Never gonna make you cry
+      // Never gonna say goodbye
+      // Never gonna tell a lie and hurt you
+      // Never gonna give you up
+      // Never gonna let you down
+      // Never gonna run around and desert you
+      // Never gonna make you cry
+      // Never gonna say goodbye
+      // Never gonna tell a lie and hurt you
+      // We've known each other for so long
+      // Your heart's been aching, but you're too shy to say it
+      // Inside, we both know what's been going on
+      // We know the game and we're gonna play it
+      // I just wanna tell you how I'm feeling
+      // Gotta make you understand
+      // Never gonna give you up
+      // Never gonna let you down
+      // Never gonna run around and desert you
+      // Never gonna make you cry
+      // Never gonna say goodbye
+      // Never gonna tell a lie and hurt you
+      // Never gonna give you up
+      // Never gonna let you down
+      // Never gonna run around and desert you
+      // Never gonna make you cry
+      // Never gonna say goodbye
+      // Never gonna tell a lie and hurt you
+      // Never gonna give you up
+      // Never gonna let you down
+      // Never gonna run around and desert you
+      // Never gonna make you cry
+      // Never gonna say goodbye
+      // Never gonna tell a lie and hurt you
         try{
             Camera.Initialise(Constants.c_Camera_ip);
         } catch(Exception e) {
