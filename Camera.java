c4bead0edb24280a93b9220fd9fee6d9cad0b9a1
Michael <mmarchiniakjr@hotmail.com>
2024-11-30 11:04:43 -0600

    add camera

---
+++ Camera.java
@@ -0,0 +1,13 @@
+public class Camera {
+    public static void main(String[]args){
+     System.out.println("Search Ethernet Interface for Camera");
+     System.out.println("Connect to camera");
+     System.out.println("Initialise camera");
+
+    }
+    public String ReadCameraPos(){
+        return  "Calculated Position: ";
+    }
+
+
+}
